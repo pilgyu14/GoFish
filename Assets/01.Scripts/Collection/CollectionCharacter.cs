e704f653c88788e62c8f18bc4c09777355a5ebd6
snowgaru <77821586+snowgaru@users.noreply.github.com>
2022-06-29 20:25:21 +0900

    0629 아직까진 구조 구상중

---
+++ Assets/01.Scripts/Collection/CollectionCharacter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite characterImage;
+
+    [SerializeField]
+    private Text characterText;
+
+    public GameObject character3d;
+    public GameObject characterInfo;
+
+
+    void Start()
+    {
+        //이미지, 텍스트 변환 시켜주기
+    }
+
+
+}
