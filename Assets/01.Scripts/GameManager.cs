ece2a748cf7acc67db4fd9dfcdd61ec538dbfdb7
snowgaru <lootmusic999@gmail.com>
2022-08-06 00:10:51 +0900

    게임 승리 및 패배 연출

--- Assets/01.Scripts/GameManager.cs
+++ Assets/01.Scripts/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : Singleton<GameManager>
     protected override void Awake()
     {
         base.Awake();
-        DontDestroyOnLoad(this);
+        //DontDestroyOnLoad(this);
     }
 
     public void HumanHomeHP(float decrease)
