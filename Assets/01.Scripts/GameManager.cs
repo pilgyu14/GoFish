05d1a340f138c557e0470daeb4501ad9cbe83add
snowgaru <lootmusic999@gmail.com>
2022-08-05 19:38:39 +0900

    게임승리 및 패배 구현

--- Assets/01.Scripts/GameManager.cs
+++ Assets/01.Scripts/GameManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    [SerializeField]
+    private GameObject panel;
+    [SerializeField]
+    private GameObject win;
+    [SerializeField]
+    private GameObject lose;
+    [SerializeField]
+    private GameObject loadtitleB;
 
     public float humanHomeHP { get; set; }
     public float fishHomeHP { get; set; }
@@ -42,11 +53,17 @@ public class GameManager : MonoBehaviour
 
     public void PlayerGameOver()
     {
-
+        Time.timeScale = 0;
+        panel.GetComponent<Image>().DOFade(0.8f, 1).SetUpdate(true);
+        lose.transform.DOLocalMoveY(170, 1f).SetUpdate(true);
+        loadtitleB.transform.DOLocalMoveY(-145, 1f).SetUpdate(true);
     }
 
     public void PlayerGameWin()
     {
-
+        Time.timeScale = 0;
+        panel.GetComponent<Image>().DOFade(0.8f, 1).SetUpdate(true);
+        win.transform.DOLocalMoveY(170, 1f).SetUpdate(true);
+        loadtitleB.transform.DOLocalMoveY(-145, 1f).SetUpdate(true);
     }
 }
